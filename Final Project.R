35420984bf1749c2036bb3732e7925ad4ffe8efa
Nathan Yung <nathanyung101@gmail.com>
2024-02-19 14:14:11 -0800

    update

--- Final Project.R
+++ Final Project.R
@@ -1,2 +1,7 @@
 states <- read.csv("scrubbed.csv")
 party <- read.csv("Party Leaning.csv")
+
+
+states$date.posted <- as.Date(states$date.posted, format="%m/%d/%Y")
+states <- states %>%
+  filter(date.posted >= as.Date("2011-01-01") & country == "us")
